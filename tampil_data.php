dd802723232ba288634d0d04bc158638324db340
Fadhila-Zahra <zahrafadhila2907@gmail.com>
2022-04-04 18:17:26 +0700

    Menambah bootstrap pada file tampil_data.php

--- tampil_data.php
+++ tampil_data.php
@@ -6,46 +6,62 @@ $sql="SELECT * FROM tbl_059";
 $hasil = mysqli_query($koneksi,$sql);
 ?>
 
+<!-- bootstrap -->
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Menampilkan data</title>
-    <style>
-      table {
-        border-collapse: collapse;
-      }
-      table,
-      th,
-      td {
-        border: 2px solid black;
-      }
-    </style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <title>Tabel Data</title>
   </head>
   <body>
-    <center>
-      <h1><strong>Tabel</strong></h1>
+    <nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: #6f42c1">
+      <div class="container">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active btn" aria-current="page" style="background-color: white; color: #6e42c1; font-weight: bold" href="#Home">Tampil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="tambah_data.php">Tambah</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container mt-4" id="Home">
+      <h2 style="color: #6e42c1">Tabel Tampil Data</h2>
       <br />
-      <table width="1000" cellpadding="10">
-        <tr style="background-color: aqua; font-size: 18px">
-          <th width="200px"><center>No Urut</center></th>
-          <th width="200px"><center>Nama</center></th>
-          <th width="200px"><center>No Hp</center></th>
-          <th width="200px"><center>Asal Kota</center></th>
-          <th width="200x"><center>Aksi</center></th>
-        </tr>
+      <table class="table table-striped table-bordered border-white text-center text-white" style="width: 100%; background-color: #6e42c1">
+        <thead>
+          <tr>
+            <th scope="col" width="20%">No Ururt</th>
+            <th scope="col" width="20%">Nama</th>
+            <th scope="col" width="20%">No HP</th>
+            <th scope="col" width="20%">Asal Kota</th>
+            <th scope="col" width="20%">Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
         <?php while($row = mysqli_fetch_array($hasil)) : ?>
-        <tr style="text-align: center">
-          <td> <?= $row["no_urut"]; ?> </td>
-          <td> <?= $row["nama"]; ?> </td>
-          <td> <?= $row["no_hp"]; ?> </td>
-          <td> <?= $row["asal_kota"]; ?> </td>
-          <td><a href="update_data1.php?no_urut=<?=$row['no_urut']?>">Perbarui</a> | <a href="hapus_data.php?id=<?=$row['no_urut']?>">Hapus</a></td>
-        </tr>
-        <?php endwhile; ?>
+          <tr>
+            <td class="text-white"><?= $row["no_urut"]; ?></td>
+            <td class="text-white"><?= $row["nama"]; ?></td>
+            <td class="text-white"><?= $row["no_hp"]; ?></td>
+            <td class="text-white"><?= $row["asal_kota"]; ?></td>
+            <td scope="row">
+              <a href="hapus_data.php?id=<?=$row['no_urut']?>"><button type="button" class="btn btn-danger" style="border: 1px solid white; font-weight: bold">Hapus</button></a>
+              <a href="update_data1.php?no_urut=<?=$row['no_urut']?>"><button type="button" class="btn btn-success" style="border: 1px solid white; font-weight: bold">Perbarui</button></a>
+            </td>
+          </tr>
+          <?php endwhile; ?>
+        </tbody>
       </table>
-    </center>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
